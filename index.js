6b8c4f665df2cb3ef42c0894ba12f6f6b0812770
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-02-26 13:39:41 +0600

    m rakib Ahmed

--- index.js
+++ index.js
@@ -23,18 +23,16 @@ const displayVideo = (video)=>{
     video.forEach(video => {
         console.log(video);
         const card = document.createElement('div');
+        card.classList = "card card-compact"
         card.innerHTML = `
-        <div class="card card-compact bg-base-100 shadow-xl">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+       
+  <figure class="h-[200px]">
+    <img class="h-full w-full object-cover"
+      src=${video.thumbnail}
       alt="Shoes" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2">
+   
     </div>
   </div>
 </div>
