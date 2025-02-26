4b9f27cd514470843631d91e9d865207ddcc509c
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-02-26 13:30:37 +0600

    thired commit

--- index.js
+++ index.js
@@ -8,13 +8,49 @@ const loadCategories = ()=>{
         .catch(error => console.log(error))
     
 }
+const loadVideos = ()=>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+        .then(data => displayVideo(data.videos))
+        .catch(error => console.log(error))
+    
+}
+
+const displayVideo = (video)=>{
+    const videoContainer = document.querySelector('#videos')
+    console.log(video);
+    // create card here
+    video.forEach(video => {
+        console.log(video);
+        const card = document.createElement('div');
+        card.innerHTML = `
+        <div class="card card-compact bg-base-100 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        
+        `;
+        videoContainer.append(card)
+        
+    });
+    
+}
 
 
 // Create DisplayCategories
 const displayCategories = (categories)=>{
     const categoryContainer = document.querySelector('#categories');
     categories.forEach((item)=> {
-        console.log(item);
         // create a button
         const button = document.createElement('button')
         button.classList = "btn";
@@ -28,3 +64,4 @@ const displayCategories = (categories)=>{
 
 
 loadCategories();
+loadVideos();
